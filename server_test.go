fee2895eb0fe8227cf1e36e2a4cc942c64c5ced6
Matthew Borders <mborders@outlook.com>
2019-01-12 18:10:37 -0600

    Added unit tests for sending/reading JSON

--- server_test.go
+++ server_test.go
@@ -1,6 +1,8 @@
 package requiem
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"testing"
 	"time"
@@ -11,7 +13,8 @@ import (
 type TestController struct {
 }
 
-type TestResponse struct {
+type TestRequest struct {
+	Message string
 }
 
 func (c TestController) Load(router *Router) {
@@ -21,20 +24,29 @@ func (c TestController) Load(router *Router) {
 	})
 
 	r.Post("/two", func(ctx HTTPContext) {
-		ctx.SendStatus(http.StatusAccepted)
-	}, TestResponse{})
+		req := ctx.Body.(*TestRequest)
+		ctx.SendJSON(req)
+	}, TestRequest{})
 }
 
 func TestNewServer(t *testing.T) {
 	s := NewServer(TestController{})
 	go s.Start()
 
-	timer := time.NewTimer(time.Second)
+	timer := time.NewTimer(time.Millisecond * 100)
 	<-timer.C
 
+	// Verify endpoint one
 	res, _ := http.Get("http://localhost:8080/api/test/one")
 	assert.Equal(t, http.StatusNoContent, res.StatusCode, "GET should return 204 status")
 
-	res, _ = http.Post("http://localhost:8080/api/test/two", "application/json", nil)
-	assert.Equal(t, http.StatusAccepted, res.StatusCode, "POST should return 202 status")
+	// Verify endpoint two
+	m := "Hello"
+	b, _ := json.Marshal(TestRequest{Message: m})
+	res, _ = http.Post("http://localhost:8080/api/test/two", "application/json", bytes.NewReader(b))
+
+	var result TestRequest
+	json.NewDecoder(res.Body).Decode(&result)
+
+	assert.Equal(t, m, result.Message, "POST should have expected response body")
 }
